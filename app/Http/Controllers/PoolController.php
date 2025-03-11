13bf2afdca4b649c230b968fb135ced3eef6dd0a
TegaEth <ochiojie@gmail.com>
2025-03-11 13:53:41 +0200

    created controller for the pool

--- app/Http/Controllers/PoolController.php
+++ app/Http/Controllers/PoolController.php
@@ -6,11 +6,19 @@ use Illuminate\Http\Request;
 
 class PoolController extends Controller
 {
-    public function create(Request $request){
+    public function createPool(Request $request){
         $request->validate([
            'name' => 'required|string|max:255',
            'amount' => 'required|numeric|min:0', 
            'cycle_days' => 'required|integer|min:1',
         ]);
+    }
+
+    public function listPools(){
+        //
+    }
+
+    public function showPool(){
+        //
     }
 }
