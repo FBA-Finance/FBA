af570c855a0ac241e96fd7c1916b1b5fae410e6c
TegaEth <ochiojie@gmail.com>
2025-03-11 12:33:36 +0200

    edited users model and added pool model

---
+++ database/migrations/2025_03_11_102431_create_pools_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pools', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('amount', 10, 2);
+            $table->integer('cycle_days');
+            $table->unsignedBigInteger('creator_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pools');
+    }
+};
